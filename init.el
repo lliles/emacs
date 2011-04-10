7ebb951eb4201e2d7493d9e3c32f67a767cec39f
Lane Liles <lane.liles@gmail.com>
2011-04-10 16:40:38 -0400

    setup basic structure

--- init.el
+++ init.el
@@ -3,4 +3,20 @@
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
+;; load path
+(setq dotfiles-dir (file-name-directory
+                    (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+;; custom code
+(load "lliles/settings.el")
+
+;; ELPA (this will be part of emacs v24)
+(setq package-user-dir (concat dotfiles-dir "elpa"))
+(require 'package)
+(dolist (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
+                  ("elpa" . "http://tromey.com/elpa/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
 
---
+++ lliles/settings.el
@@ -0,0 +1,6 @@
+;; turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+

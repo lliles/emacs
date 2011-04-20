dfe861ee2de7be0d5ba23cd77c560d1835a8e207
Lane Liles <lane.liles@gmail.com>
2011-04-19 21:47:24 -0400

    remove dupe message

--- custom/vendor.el
+++ custom/vendor.el
@@ -7,4 +7,4 @@
      ((file-directory-p normal) (add-to-list 'load-path normal) (require library))
      ((file-directory-p suffix) (add-to-list 'load-path suffix) (require library))
      ((file-exists-p suffix) (require library)))
-    (when (file-exists-p custom) (message (concat "loading " dotfiles-dir)) (load custom))))
+    (when (file-exists-p custom) (load custom))))

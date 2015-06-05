e2751d52ccde96819327794282a0c2cb642b0d80
Lane Liles <lane.liles@gmail.com>
2015-06-05 16:35:33 -0400

    remove old elpa repo

--- lliles/custom/package.el
+++ lliles/custom/package.el
@@ -5,8 +5,7 @@
 ;; package sources
 (dolist 
   (source '(("marmalade" . "http://marmalade-repo.org/packages/")
-            ("melpa"     . "http://melpa.milkbox.net/packages/")
-            ("elpa"      . "http://tromey.com/elpa/")))
+            ("melpa"     . "http://melpa.milkbox.net/packages/")))
   (add-to-list 'package-archives source t))
 
 (package-initialize)

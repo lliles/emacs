bfe2e0b72079583658749fbb1458fb1b9a76eb6f
Lane Liles <lane.liles@gmail.com>
2016-03-16 11:45:36 -0400

    entries for loading new themes

--- lliles/custom/themes.el
+++ lliles/custom/themes.el
@@ -4,16 +4,31 @@
 ;      (solarized-termcolors 256))
 ;  (load-theme 'solarized-dark t))
 
-;; zenburn
-;(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
-;(load-theme 'zenburn t)
+;; zenburn & hc-zenburn
+;; (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes"))
+;; (load-theme 'zenburn t)
+;; (load-theme 'hc-zenburn t)
 
 ;; dark-laptop
 ;(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/dark-laptop"))
 ;(load-theme 'dark-laptop t)
 
 ;; tomorrow-theme
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/tomorrow-theme"))
-(add-to-list 'load-path (concat user-emacs-directory "themes/tomorrow-theme"))
-(load-theme 'tomorrow-night t)
+;; (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/tomorrow-theme"))
+;; (add-to-list 'load-path (concat user-emacs-directory "themes/tomorrow-theme"))
+;; (load-theme 'tomorrow-night t)
+
+;; sanityinc-tomorrow
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/sanityinc-tomorrow"))
+(add-to-list 'load-path (concat user-emacs-directory "themes/sanityinc-tomorrow"))
+(load-theme 'sanityinc-tomorrow-night t)
+
+;; base16-theme
+;; (add-to-list 'custom-theme-load-path (concat user-emacs-directory "themes/base16-emacs"))
+;; (add-to-list 'load-path (concat user-emacs-directory "themes/base16-emacs"))
+;; (load-theme 'base16-default-dark t)
+;; (load-theme 'base16-colors-dark t)
+;; (load-theme 'base16-flat-dark t)
+;; (load-theme 'base16-eighties-dark t)
+;; (load-theme 'base16-tomorrow-dark t)
 

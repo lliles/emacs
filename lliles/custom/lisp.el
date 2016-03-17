6d9c6365915f41e887d593fedf605facc9d1cca4
Lane Liles <lane.liles@gmail.com>
2016-03-17 16:41:51 -0400

    remove compilation function

--- lliles/custom/lisp.el
+++ lliles/custom/lisp.el
@@ -5,17 +5,7 @@
 (define-key lisp-mode-shared-map (kbd "C-c v") 'eval-buffer)
 
 ;;; Emacs Lisp
-
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'esk-remove-elc-on-save)
-
-(defun esk-remove-elc-on-save ()
-  "If you're saving an elisp file, likely the .elc is no longer valid."
-  (make-local-variable 'after-save-hook)
-  (add-hook 'after-save-hook
-            (lambda ()
-              (if (file-exists-p (concat buffer-file-name "c"))
-                  (delete-file (concat buffer-file-name "c"))))))
 
 (define-key emacs-lisp-mode-map (kbd "M-.") 'find-function-at-point)
 

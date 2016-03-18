b7ec70395d35413e7afea806013cea394117c1e7
Lane Liles <lane.liles@gmail.com>
2016-03-18 08:51:14 -0400

    remove unused PATH manipulations in silversurfer.el

--- lliles/custom/silversurfer.el
+++ lliles/custom/silversurfer.el
@@ -1,2 +1,1 @@
-(setenv "PATH" (concat "/usr/local/Cellar/git/1.8.1.3/bin:" (getenv "PATH") ":/usr/local/bin:~/.rvm/bin"))
-(setq exec-path (append "/usr/local/Cellar/git/1.8.1.3/bin:" exec-path "/usr/local/bin" "~/.rvm/bin:"))
+
